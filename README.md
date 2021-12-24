ab6b06ec56a1f34e1d1ab130d467e2e2b9c0f1a2
Leo Dickens <dickens.leo@gmail.com>
2021-12-23 23:19:56 -0600

    js bugs

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # homeworkFiveLeoD
+Added a form formatting to the html
+attemting to debug javascript
--- homework_5/Develop/script.js
+++ homework_5/Develop/script.js
@@ -1,3 +1,15 @@
-var data = [
+let saveBtn = document.getElementById('save');
+let entry = document.getElementById('8am');
 
-];
+entry.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    saveBtn.addEventListener('click',(e) => {
+        e.preventDefault();
+        let submission = document.getElementById('8am');
+        console.log(submission)
+        
+        submission = submission.value;
+        localStorage.setItem('8AM',submission)
+})
+});
