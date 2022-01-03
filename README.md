967f52f8ca11ed87912f1a003a67db4041dd93bb
Leo Dickens <dickens.leo@gmail.com>
2022-01-03 01:35:19 -0600

    added functionality

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ attemting to debug javascript
 made several attempts at rebuilding the javascript. kept failing to save input to local storage.
 attempting to revisit assignment and debug javascript
 removed classes and ids from html items
+cleaned up html further, removed corresponding css and js
--- homework_5/Develop/script.js
+++ homework_5/Develop/script.js
@@ -1,8 +1,18 @@
+var submitButton = document.querySelector("#btn");
+var inputArea = document.querySelector("#input-task");
+var responseArea = document.querySelector("#submitted-task");
+var resetForm = document.getElementById('myform').reset();
 
 
 
-
-
+function postTask (e) {
+    e.preventDefault();
+    let tasked = inputArea.value;
+    console.log(tasked)
+    responseArea.textContent = tasked;
+    return(resetForm);
+};
+submitButton.addEventListener("click", postTask);
 // var saveBtn = document.getElementById('save');
 // var entry = document.getElementById('8am');
 
