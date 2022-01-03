3aaa9dd83f57fd4a6f61a0a14c4fbe566f995bc5
Leo Dickens <dickens.leo@gmail.com>
2022-01-02 21:27:04 -0600

    cleanup

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Added a form formatting to the html
 attemting to debug javascript
 made several attempts at rebuilding the javascript. kept failing to save input to local storage.
 attempting to revisit assignment and debug javascript
+removed classes and ids from html items
--- homework_5/Develop/script.js
+++ homework_5/Develop/script.js
@@ -1,16 +1,21 @@
-var saveBtn = document.getElementById('save');
-var entry = document.getElementById('8am');
 
 
-    saveBtn.addEventListener('click',(e) => {
-        e.preventDefault();
-        var submission = document.getElementById('8AM');
 
-        if(localStorage.getItem('8AM')== null){
-            localStorage.setItem('8AM', '[]');
-        }
+
+
+// var saveBtn = document.getElementById('save');
+// var entry = document.getElementById('8am');
+
+
+//     saveBtn.addEventListener('click',(e) => {
+//         e.preventDefault();
+//         var submission = document.getElementById('8AM');
+
+//         if(localStorage.getItem('8AM')== null){
+//             localStorage.setItem('8AM', '[]');
+//         }
         
-        submission = submission.value;
-        localStorage.setItem('8AM',submission)
-});
+//         submission = submission.value;
+//         localStorage.setItem('8AM',submission)
+// });
 
